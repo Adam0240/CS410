2d5cc28fd88b6b86c3c8e4ba951e79be36232372
ChByrne1 <cbyrne1@wvup.edu>
2026-02-20 22:51:42 -0500

    Comments in Character, Player, in Protag
    [SCRUM-75] Added comments to Player, Character, and Protagonist to improve readability and comprehensibility.

--- ConsoleApp_121_FinalProjectShell/People/Player.cs
+++ ConsoleApp_121_FinalProjectShell/People/Player.cs
@@ -7,76 +7,79 @@ using ConsoleApp_121_FinalProjectShell.Core;
 
 namespace ConsoleApp_121_FinalProjectShell.People;
 
-/**
-* This class is part of the "FUGwACL Adventure" application.
-* "FUGwACL Adventure" is a slightly less simple, text based adventure game.
-*
-* Used to keep track of both the player and the protagonist.
-* Has an arraylist to hold items with a weight limit,
-* and tracks the order of rooms visited for the BACK command.
-*
-* @author  Michael Kölling, David J. Barnes, and Christian Byrne
-* @version 2023-12-03
-*/
+
+/// <summary>
+/// Manages traits and actions unique to the player character, such as item inventory.
+/// Holds items in an arraylist and manages it using a weight limit.
+/// Would have much more functionality in an actually complex game. 
+///</summary>
 public class Player : Character
 {
-    
-    private ArrayList inventory;
-    private int carryWeight;
-    private int currentWeight;
-
-    //bug: had to change the field name - adam
-    
+    //inventory-based parameters
+    //may be tied into an interface in the future
+    private ArrayList _inventory;
+    private int _carryWeight;
+    private int _currentWeight;
 
     
-    //basic constructor
-    //does not initialize Steps if it's not the protag
+    /// <summary>
+    /// Creates a new instance of Player and initializes default values.
+    /// Barring testing there should only be one of these in a given game.
+    /// </summary>
     public Player() : base()
     {
-        inventory = new ArrayList();
-        carryWeight = 100;
-        currentWeight = 0;
-        
+        _inventory = new ArrayList();
+        _carryWeight = 100;
+        _currentWeight = 0;
     }
-
-    //accessors and mutators for the weights
-    public int getCurrentWeight()
-    {
-        return currentWeight;
-    }
-
-    public int getCarryWeight()
-    {
-        return carryWeight;
-    }
-
-    public void setCarryWeight(int number)
-    {
-        carryWeight = number;
-    }
-
     
-    //called after adding or removing items from the inventory
-    public void updateCarryWeight()
+    /// <summary>
+    /// Overloaded constructor for also initializing a starting room.
+    /// </summary>
+    /// <param name="startroom">The room to place the instance of Player in.</param>
+    public Player(Room startroom) : base(startroom)
+    {
+        _inventory = new ArrayList();
+        _carryWeight = 100;
+        _currentWeight = 0;
+    }
+
+    //Accessors and Mutators 
+    public int getCurrentWeight() { return _currentWeight; }
+    
+    public int getCarryWeight() { return _carryWeight; }
+    
+    public void setCarryWeight(int number) { _carryWeight = number; }
+
+    /// <summary>
+    /// Called when adding or removing items from the inventory, recalculates current total inventory weight
+    /// Used for consistency purposes to ensure nothing else ever has to update it.
+    /// </summary>
+    private void updateCarryWeight()
     {
         int tempweight = 0;
-        foreach (Item item in inventory)
+        foreach (Item item in _inventory)
         
             tempweight += item.getWeight();
         
-        currentWeight = tempweight;
+        _currentWeight = tempweight;
     }
 
     //returns description of items held. called by ITEMS command
+    /// <summary>
+    /// Returns the list of descriptions of every item held in the inventory, exclusively called when using the ITEMS
+    /// command in-game.
+    /// </summary>
+    /// <returns>A string description of the Player object's inventory.</returns>
     public string itemsText()
     {
         var iText = new System.Text.StringBuilder();
         iText.Append("You are holding");
 
-        if (inventory.Count > 0)
+        if (_inventory.Count > 0)
         {
             iText.Append(":");
-            foreach (Item item in inventory)
+            foreach (Item item in _inventory)
                 iText.Append("  " + item.getName());
             iText.AppendLine();
         }
@@ -86,14 +89,18 @@ public class Player : Character
             
         }
 
-        iText.Append("Current weight: " + currentWeight);
+        iText.Append("Current weight: " + _currentWeight);
         return iText.ToString();
     }
 
-    //Helper method
+    /// <summary>
+    /// Helper method that keeps inventory searching contained to a single method to prevent duplication.
+    /// </summary>
+    /// <param name="name">The name of the item to find.</param>
+    /// <returns>The instance of Item with a matching name, or null if there is none.</returns>
     private Item findItem(string name)
     {
-        foreach (Item item in inventory)
+        foreach (Item item in _inventory)
         {
             if (item.getName().Equals(name, StringComparison.OrdinalIgnoreCase))
                 return item;
@@ -101,34 +108,54 @@ public class Player : Character
         return null;
     }
 
-    //methods for interaction with inventory
+    /// <summary>
+    /// Uses findItem() to check if an Item is in the player's inventory. 
+    /// </summary>
+    /// <param name="name">The name of the item to look for.</param>
+    /// <returns>True if the named item is in the inventory, false otherwise. </returns>
     public bool hasItemByName(string name)
     {
         return findItem(name) != null;
     }
 
+    /// <summary>
+    /// Uses findItem() to fetch a named Item from the player's inventory. Note that this does not  remove the Item
+    /// from the inventory.
+    /// </summary>
+    /// <param name="name">The name of the item to retrieve.</param>
+    /// <returns>The object instance of the item needed, or null if it doesn't exist.</returns>
     public Item getItemByName(string name)
     {
         return findItem(name);
     }
 
-
+    /// <summary>
+    /// Uses findItem() to identify a named Item in the player's inventory and removes it.
+    /// Does not return the removed item.
+    /// </summary>
+    /// <param name="name">The name of the item to remove.</param>
     public void removeItemByName(string name)
     {
         Item item = findItem(name);
         if (item != null)
         {
-            inventory.Remove(item);
+            _inventory.Remove(item);
             updateCarryWeight();
         }
     }
 
+    /// <summary>
+    /// Takes in an Item and adds it to the player's inventory. Weight checking is done externally (to allow for it to
+    /// potentially be bypassed for some reason)>
+    /// </summary>
+    /// <param name="item">The Item to add to the player's inventory.</param>
     public void addItem(Item item)
     {
-        inventory.Add(item);
+        _inventory.Add(item);
         updateCarryWeight();
     }
-    //returns true if something with a weight of itemWeight can be added to inventory
+    
+    //Helper method that tells whether something can be added to the inventory without overcapping the weight limit.
     public bool weightCheck(int itemWeight)
     {
         return getCarryWeight() >= getCurrentWeight() + itemWeight;
--- ConsoleApp_121_FinalProjectShell/People/Protagonist.cs
+++ ConsoleApp_121_FinalProjectShell/People/Protagonist.cs
@@ -3,6 +3,10 @@ using ConsoleApp_121_FinalProjectShell.Core;
 
 namespace ConsoleApp_121_FinalProjectShell.People;
 
+/// <summary>
+/// Manages traits and actions unique to the Protagonist character, a unique kind of NPC tied to progression.
+/// They are the bumbling fool the player needs to set up to have the best possible chance for success.
+///</summary>
 public class Protagonist : Character
 {
     private int protagStepsCount;
@@ -13,7 +17,7 @@ public class Protagonist : Character
     }
     
     
-    //moves the protagonist. called after every command.
+    //Moves the protagonist through a random room exit. Very simple function.
     public bool protagSteps(Command command)
     {
         if (protagStepsCount >= 8)
@@ -27,7 +31,7 @@ public class Protagonist : Character
         return false;
     }
     
-    //Method added for testing
+    //Method needed for testing purposes. 
     public int getProtagStepsCount()
     {
         return protagStepsCount;
