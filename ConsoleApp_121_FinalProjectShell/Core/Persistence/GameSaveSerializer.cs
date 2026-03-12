b595577a2fded8b2b4ba9c2908fe7eb3c75fb4a9
Adam Abbadusky <adam.adusky@gmail.com>
2026-03-12 12:38:24 -0400

    SCRUM-93 started save state (unfinished)

---
+++ ConsoleApp_121_FinalProjectShell/Core/Persistence/GameSaveSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp_121_FinalProjectShell.Core.Persistence;
+
+public static class GameSaveSerializer
+{
+    private static readonly JsonSerializerOptions Options = new()
+    {
+        WriteIndented = true
+    };
+
+    public static string ToJson(GameSaveState state) =>
+        JsonSerializer.Serialize(state, Options);
+
+    public static GameSaveState? FromJson(string json) =>
+        JsonSerializer.Deserialize<GameSaveState>(json, Options);
+}
---
+++ ConsoleApp_121_FinalProjectShell/Core/Persistence/IGameSaveRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp_121_FinalProjectShell.Core.Persistence;
+
+public interface IGameSaveRepository
+{
+    void Initialize();
+    void SaveJson(string saveJson);
+    string? LoadJson();
+}
